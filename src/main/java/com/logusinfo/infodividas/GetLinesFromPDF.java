0f53732efd146fe316394c7698138c8d4a9d65ea
Masaru Ohashi Júnior <masaru.ohashi@gmail.com>
2023-10-27 12:20:50 -0300

    leitor linhas

---
+++ src/main/java/com/logusinfo/infodividas/GetLinesFromPDF.java
@@ -0,0 +1,61 @@
+package com.logusinfo.infodividas;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.TextPosition;
+
+public class GetLinesFromPDF
+  extends PDFTextStripper {
+
+  static List<String> lines = new ArrayList<String>();
+
+  public GetLinesFromPDF()
+    throws IOException {
+  }
+
+  /**
+   * @throws IOException If there is an error parsing the document.
+   */
+  public static void main(String[] args)
+    throws IOException {
+    PDDocument document = null;
+    String fileName = "C:\\Users\\Masaru\\Downloads\\RPV___MARIA_DE_JESUS_MELO.pdf";
+    try {
+      document = PDDocument.load(new File(fileName));
+      PDFTextStripper stripper = new GetLinesFromPDF();
+      stripper.setSortByPosition(true);
+      stripper.setStartPage(0);
+      stripper.setEndPage(document.getNumberOfPages());
+
+      Writer dummy = new OutputStreamWriter(new ByteArrayOutputStream());
+      stripper.writeText(document, dummy);
+
+      // print lines
+      for (String line : lines) {
+        System.out.println(line);
+      }
+    } finally {
+      if (document != null) {
+        document.close();
+      }
+    }
+  }
+
+  /**
+   * Override the default functionality of PDFTextStripper.writeString()
+   */
+  @Override
+  protected void writeString(String str, List<TextPosition> textPositions)
+    throws IOException {
+    lines.add(str);
+    // you may process the line here itself, as and when it is obtained
+  }
+}
